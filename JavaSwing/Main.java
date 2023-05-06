5f38001bc655a560945603a988cc8436b0d79504
EderA117 <113396905+EderA117@users.noreply.github.com>
2023-05-06 16:17:58 -0600

    Add files via upload

---
+++ JavaSwing/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        //Ventana ventana = new Ventana("Layouts");
+
+        //VentanaGrid ventana = new VentanaGrid("Ventana Layout Cuadricula");
+
+        VentanaSwing ventanaSwing = new VentanaSwing("Centigrados a Farenheid");
+    }
+}
