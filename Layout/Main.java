77e8e02eb3a698ebee53fea45d7ec07cb32438fc
EderA117 <113396905+EderA117@users.noreply.github.com>
2023-05-06 16:11:18 -0600

    Add files via upload

---
+++ Layout/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        //Ventana ventana = new Ventana("Layouts");
+
+        VentanaGrid ventana = new VentanaGrid("Ventana Layout Cuadricula");
+    }
+}
